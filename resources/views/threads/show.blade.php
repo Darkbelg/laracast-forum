ad07e5d407195c7b8e81472f7a40f0c987844a91
Darkbelg <stijn.sagaert@outlook.com>
2020-07-20 22:31:59 +0200

    Episode 16 - Meta Details and Pagination

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">
@@ -14,19 +14,14 @@
                     {{ $thread->body }}
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            @foreach ($thread->replies as $reply)
+
+            @foreach ($replies as $reply)
             @include('threads.reply')
             @endforeach
-        </div>
-    </div>
 
-    @auth
-    <div class="row justify-content-center">
-        <div class="col-md-8 mt-3">
+            {{ $replies->links()}}
+
+            @auth
             <form method="POST" action="{{ $thread->path() . '/replies'}}">
                 {{ csrf_field() }}
 
@@ -37,10 +32,21 @@
 
                 <button type="submit" class="btn btn-primary">Post</button>
             </form>
+            @else
+            <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to participate in this discussion.
+            </p>
+            @endauth
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <p>
+                        This thread was published {{ $thread->created_at->diffForHumans()}} by
+                        <a href="#"> {{$thread->creator->name }}</a>, and currently has {{ $thread->replies_count}}
+                        {{ Str::plural('comment', $thread->replies_count)}}.
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
-    @else
-    <p class="text-center">Please <a href="{{route('login')}}">sign in</a> to participate in this discussion.</p>
-    @endauth
-</div>
-@endsection
+    @endsection
