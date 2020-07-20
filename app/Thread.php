acbc036cb31b41ec4ba8f9cc19f7a4cb67635370
Darkbelg <stijn.sagaert@outlook.com>
2020-07-20 22:00:33 +0200

    Episode 15 - A Lesson in Refactoring

--- app/Thread.php
+++ app/Thread.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Filters\ThreadFilters;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
@@ -21,15 +23,21 @@ class Thread extends Model
 
     public function creator()
     {
-        return $this->belongsTo(User::class,'user_id');
+        return $this->belongsTo(User::class, 'user_id');
     }
 
     public function channel()
     {
         return $this->belongsTo(Channel::class);
     }
+    
     public function addReply($reply)
     {
         $this->replies()->create($reply);
+    }
+
+    public function scopeFilter($query, ThreadFilters $filters)
+    {
+        return $filters->apply($query);
     }
 }
