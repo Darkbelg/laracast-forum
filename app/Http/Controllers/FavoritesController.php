929802b4fa45b1b82c9878ea1bee20dda0923e34
Darkbelg <stijn.sagaert@outlook.com>
2020-07-21 22:21:50 +0200

    Episode 18 - A User Can Favorite Replies

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Favorite;
+use App\Reply;
+use Illuminate\Http\Request;
+
+class FavoritesController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        return $reply->favorite();
+        
+        //another way
+        // Favorite::create([
+        //     'user_id' => auth()->id(),
+        //     'favorited_id' => $reply->id,
+        //     'favorited_type' => get_class($reply)
+        // ]);
+
+        //fastest way
+        // return \DB::table('favorites')->insert([
+        //     'user_id' => auth()->id(),
+        //     'favorited_id' => $reply->id,
+        //     'favorited_type' => get_class($reply)
+        // ]);
+    }
+}
---
+++ tests/Feature/FavoritesTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Exception;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FavoritesTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    public function test_a_guest_can_not_favorite_anything()
+    {
+        $this->post('replies/1/favorites')
+            ->assertRedirect('/login');
+    }
+
+
+    public function test_an_authenticated_user_can_favorite_any_reply()
+    {
+        $this->signIn();
+
+        $this->withoutExceptionHandling();
+
+        $reply = create('App\Reply');
+
+        $this->post('replies/' . $reply->id . '/favorites');
+
+        $this->assertCount(1, $reply->favorites);
+    }
+
+    public function test_an_authenticated_user_may_only_favorite_a_reply_once()
+    {
+
+        $this->withoutExceptionHandling();
+        $this->signIn();
+
+        $reply = create('App\Reply');
+
+        try {
+            $this->post('replies/' . $reply->id . '/favorites');
+            $this->post('replies/' . $reply->id . '/favorites');
+        } catch (Exception $e) {
+            $this->fail('Did not expect to insert the same record twice.');
+        }
+
+        $this->assertCount(1, $reply->favorites);
+    }
+}
