b209270b7701ca9133115bc735b35727b6fb6983
Darkbelg <stijn.sagaert@outlook.com>
2020-07-27 20:19:04 +0200

    EPISODE26HowToConstructAnActivityFeedWithTDD:Part2

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,14 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Activity;
 
 class ProfilesController extends Controller
 {
     public function show(User $user)
     {
+
+        $activities = $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity){
+            return $activity->created_at->format('Y-m-d');
+        });
+
         return view('profiles.show',[
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => $activities
         ]);
     }
 }
--- tests/Feature/ProfileTest.php
+++ tests/Feature/ProfileTest.php
@@ -20,14 +20,14 @@ class ProfileTest extends TestCase
             ->assertSee($user->name);
      }
 
-     public function test_a_profiles_display_all_threads_created_by_the_associated_user()
+     public function test_profiles_display_all_threads_created_by_the_associated_user()
      {
-         $user = create("App\User");
-
-         $thread = create("App\Thread",['user_id' => $user->id]);
+        $this->signIn();
+        
+        $thread = create("App\Thread",['user_id' => auth()->id()]);
 
          $this->withoutExceptionHandling();
-         $this->get("/profiles/{$user->name}")
+         $this->get("/profiles/" . auth()->user()->name)
          ->assertSee($thread->title)
          ->assertSee($thread->body);
 
