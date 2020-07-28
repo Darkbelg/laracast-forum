fd0ae51e13a24bee629e97629d8ba99197812a7e
Darkbelg <stijn.sagaert@outlook.com>
2020-07-28 14:29:56 +0200

    Episode 27 - ExtractingControllerQueriesToTheModel

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -10,14 +10,9 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-
-        $activities = $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function($activity){
-            return $activity->created_at->format('Y-m-d');
-        });
-
         return view('profiles.show',[
             'profileUser' => $user,
-            'activities' => $activities
+            'activities' => Activity::feed($user)
         ]);
     }
 }
