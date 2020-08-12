12a93108a48945dc09cb1afa946273db86545bb2
Stijn Sagaert <stijn.sagaert@outlook.com>
2020-08-12 16:44:30 +0200

    Episode 63 Testing Avatar Uploads

--- routes/web.php
+++ routes/web.php
@@ -21,20 +21,20 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/threads','ThreadsController@index');
-Route::get('/threads/create','ThreadsController@create');
-Route::get('/threads/{channel}/{thread}','ThreadsController@show');
-Route::delete('/threads/{channel}/{thread}','ThreadsController@destroy');
-Route::post('/threads','ThreadsController@store');
-Route::get('/threads/{channel}','ThreadsController@index');
+Route::get('/threads', 'ThreadsController@index');
+Route::get('/threads/create', 'ThreadsController@create');
+Route::get('/threads/{channel}/{thread}', 'ThreadsController@show');
+Route::delete('/threads/{channel}/{thread}', 'ThreadsController@destroy');
+Route::post('/threads', 'ThreadsController@store');
+Route::get('/threads/{channel}', 'ThreadsController@index');
 //Route::resource('threads', 'ThreadsController');
-Route::get('/threads/{channel}/{thread}/replies','RepliesController@index');
-Route::post('/threads/{channel}/{thread}/replies','RepliesController@store');
-Route::patch('/replies/{reply}','RepliesController@update');
-Route::delete('/replies/{reply}','RepliesController@destroy');
+Route::get('/threads/{channel}/{thread}/replies', 'RepliesController@index');
+Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
+Route::patch('/replies/{reply}', 'RepliesController@update');
+Route::delete('/replies/{reply}', 'RepliesController@destroy');
 
-Route::post('/threads/{channel}/{thread}/subscriptions','ThreadSubscriptionsController@store')->middleware('auth');
-Route::delete('/threads/{channel}/{thread}/subscriptions','ThreadSubscriptionsController@destroy')->middleware('auth');
+Route::post('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@store')->middleware('auth');
+Route::delete('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@destroy')->middleware('auth');
 
 Route::post('/replies/{reply}/favorites', 'FavoritesController@store');
 Route::delete('/replies/{reply}/favorites', 'FavoritesController@destroy');
@@ -43,4 +43,5 @@ Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
 Route::get('/profiles/{user}/notifications', 'UserNotificationsController@index');
 Route::delete('/profiles/{user}/notifications/{notification}', 'UserNotificationsController@destroy');
 
-Route::get('api/users','Api\UsersController@index');
+Route::get('api/users', 'Api\UsersController@index');
+Route::post('api/users/{user}/avatar', 'Api\UserAvatarController@store')->middleware('auth');
